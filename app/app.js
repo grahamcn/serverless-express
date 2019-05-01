60e8754b14760ec14f5e17462848d8e178377031
Graham Neilson <graham.neilson@tyresonthedrive.com>
2019-05-01 12:04:41 +0100

    example totd BE request

--- app/app.js
+++ app/app.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
+
+app.get('/hubs/:postcode', (req, res) => {
+  const {postcode} = req.params
+
+  const options = {}
+  options.responseType = 'json'
+  options.url = `https://frontend.staging.tyresonthedrive.com/api/v1/hubs?postcode=${postcode}`
+  options.headers = {
+    'x-otd-clientid': 'totd'
+  }
+
+  axios(options)
+    .then(({ data }) =>
+      res.json(data)
+    ).catch(error => {
+      console.log('/hubId/:postcode error', error)
+      res.sendStatus(500)
+    })
+})
+
 
 app.get('*', (req, res) => {
   res.json({
-    message: 'Hi'
+    message: 'Hello world'
   })
 })
 
